867d9883cedf41cb3cc52718390e321087710662
Lucas <167575443+raul38GGez@users.noreply.github.com>
2025-08-18 14:37:48 -0300

    Update paginaip.js

--- static/js/paginaip.js
+++ static/js/paginaip.js
@@ -1,6 +1,6 @@
 
-const ipPython  = "http://127.0.0.1:5000"
-const ipPython2 = "http://127.0.0.1:5000"
+const ipPython  = "http://192.168.1.160:5000"
+const ipPython2 = "http://192.168.1.160:5000"
 // aaaaaaaaa
 // ESSA PÁGINA É ONDE FICA SALVA A VARIÁVEL RESPONSÁVEL POR
 // ARMAZENAR O URL DO IP DO SERVIDOR
